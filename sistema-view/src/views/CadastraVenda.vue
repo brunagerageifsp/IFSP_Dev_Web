1dda29739dd8141f9fdb949afcdc3b3785ffd175
Bruna Gerage <catena.bbarros@hubfintech.com.br>
2023-05-12 20:07:51 -0300

    Subindo totalVendas

--- sistema-view/src/views/CadastraVenda.vue
+++ sistema-view/src/views/CadastraVenda.vue
@@ -7,34 +7,39 @@
   
           <label class="sr-only" for="input-codbarra">Código de Barra</label>
           <b-form-input 
-            @input="listaprodutos"
+            @keypress="listaprodutos"
             id="input-codbarra"
             class="mb-2 mr-sm-2 mb-sm-0"
             placeholder="Digite aqui o Codigo de Barra do Produto"
             v-model="BuscaProduto.codbarra"
           ></b-form-input>
-
+          <br>
+          <p> Lista de Compras: </p>
+          <div v-for="produto in BuscaProduto.produtos" :key="produto" class="pill">
+            {{ ""+produto[0]+ " R$"+produto[1] }}
+          </div>
+          <br>
+          <br>
+          <br>
+          <div v-if="BuscaProduto.nome_produto">{{ "Total: " + BuscaProduto.valorTotal}}
             <br>
             <br>
             <br>
-            <div v-if="BuscaProduto.nome_produto">{{ BuscaProduto.nome_produto +"  " +"  R$ "+ BuscaProduto.preco_produto }}
-                <br>
-                <br>
-                <br>
-                <p>Forma de Pagamento</p>
-                <select v-model="BuscaProduto.formaPagamento">
-                    <option value="1">Dinheiro</option>
-                    <option value="2">Débito</option>
-                    <option value="3">Crédito</option>
-                </select>
-            </div>
-            <div v-if="BuscaProduto.preco_produto">
-                <br>
-                <b-button type="submit" variant="primary">Cadastrar Venda</b-button>
-            </div>  
+            <p>Forma de Pagamento</p>
+            <select v-model="BuscaProduto.formaPagamento">
+                <option value="1">Dinheiro</option>
+                <option value="2">Débito</option>
+                <option value="3">Crédito</option>
+            </select>
+          </div>
+            
+          <div v-if="BuscaProduto.preco_produto">
+              <br>
+              <b-button type="submit" variant="primary">Cadastrar Venda</b-button>
+          </div>  
         </b-form>
+      </div>
     </div>
-</div>
         
   </template>
   
@@ -55,6 +60,7 @@
     data(){
       return{
         BuscaProduto: {
+            produtos: [],
             formaPagamento: '1',
             codbarra: '',
             nome_produto: '',
@@ -65,20 +71,32 @@
       }
     },
     methods: {
-        listaprodutos(){
+        listaprodutos(e){
+          if (e.key === ' ' && this.BuscaProduto.codbarra){
             this.$http.get('/listaprodutos/id?id='+this.BuscaProduto.codbarra)
             .then(response => {
-                console.log(response.data);
-                console.log(response.data.preco_produto);
+                // console.log(response.data);
+                // console.log(response.data.preco_produto);
                 this.BuscaProduto.nome_produto = response.data.nome;
                 this.BuscaProduto.preco_produto = response.data.preco;
                 this.BuscaProduto.valorTotal = response.data.preco;
                 this.BuscaProduto.dataCompra = new Date().toJSON().slice(0,10);
+                let produto = [];
+                produto[0] = response.data.nome;
+                produto[1] = response.data.preco;
+                this.BuscaProduto.produtos.push(produto);
+                let quantidade = this.BuscaProduto.produtos.length;
+                let contador = 0;
+                this.BuscaProduto.valorTotal = 0;
+                while (contador < quantidade){
+                  this.BuscaProduto.valorTotal = this.BuscaProduto.valorTotal + this.BuscaProduto.produtos[contador][1];
+                  contador++;
+                }
             })
+          }
         },
 
         cadastravenda() {
-            console.log("entrou");
             let payload = this.$http.post('/cadastravenda', this.BuscaProduto)
             .then(response => {
             console.log(response);
